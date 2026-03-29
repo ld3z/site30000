04bfadc2526d799394d720ee1b718e649cc6a01c
ld3zian <42660576+ld3z@users.noreply.github.com>
2026-03-28 18:52:23 -0700

    update

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,7 +1,7 @@
 export const appName = 'Site30000';
-export const docsRoute = '/docs';
-export const docsImageRoute = '/og/docs';
-export const docsContentRoute = '/llms.mdx/docs';
+export const docsRoute = '/site30000/docs';
+export const docsImageRoute = '/site30000/og/docs';
+export const docsContentRoute = '/site30000/llms.mdx/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
